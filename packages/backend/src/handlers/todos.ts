923e6b24ba69e49d3532c63dc1654475273dc042
Krzysztof Pogorzelski <k.pogorzelski.dev@gmail.com>
2024-10-17 17:36:30 +0100

    [be-4]: Add fetch todo by id endpoint

--- packages/backend/src/handlers/todos.ts
+++ packages/backend/src/handlers/todos.ts
@@ -1,8 +1,11 @@
 import type { FastifyRequest, FastifyReply } from 'fastify'
 
-const DEFAULT_SRV_ERR_MSG = 'An error occurred while fetching todos'
+type ParamId = { id: string }
 
 export const todosHandlers = {
+  // @ GET /todos
+  // @desc Get all todos
+  // @access Public
   getTodos: async (req: FastifyRequest, res: FastifyReply) => {
     try {
       const todos = await req.server.prisma.todo.findMany()
@@ -12,7 +15,30 @@ export const todosHandlers = {
       return res.status(200).send(todos)
     } catch (error) {
       req.server.log.error(error)
-      return res.status(500).send({ message: DEFAULT_SRV_ERR_MSG })
+      return res.status(500).send({ message: 'An error occurred while fetching todos' })
+    }
+  },
+  // @ GET /todos/:id
+  // @desc Get todo by ID
+  // @access Public
+  getTodoById: async (
+    req: FastifyRequest<{
+      Params: ParamId
+    }>,
+    res: FastifyReply,
+  ) => {
+    try {
+      const todoId = req.params.id
+      if (!todoId) return res.status(400).send({ message: 'Todo ID is required' })
+
+      const todo = await req.server.prisma.todo.findUnique({ where: { id: todoId } })
+
+      if (!todo) return res.status(404).send({ message: `Todo with id: ${todoId} not found` })
+
+      return res.status(200).send(todo)
+    } catch (error) {
+      req.server.log.error(error)
+      return res.status(500).send({ message: 'An error occurred while fetching todo' })
     }
   },
 }
--- packages/backend/src/routes/todos.ts
+++ packages/backend/src/routes/todos.ts
@@ -3,6 +3,7 @@ import { todosHandlers } from '../handlers/todos'
 
 const todosRoutes = async (app: FastifyInstance) => {
   app.get('/', { handler: todosHandlers.getTodos })
+  app.get('/:id', { handler: todosHandlers.getTodoById })
 }
 
 export default todosRoutes
